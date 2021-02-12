0127a7952f2f21685adb45c9f38b70dac26dc6d2
hassoonsy2 <71430169+hassoonsy2@users.noreply.github.com>
2021-02-12 18:37:24 +0100

    18:37

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -1,6 +1,10 @@
 """ MasterMind
 
-   Hussin ALmoustafa   """
+   Hussin ALmoustafa
+
+   Studentennummer : 177645 """
+
+
 
 
 
@@ -72,12 +76,25 @@ def evaluate(gok, kleur_code):
 
 
 def Simpel_strategy(kleur_code):
+
+    """ Het Simpel Strategy werkt als volende :
+    Eerst  geef ik de eerste gok vanuit Aantal moeglijkheden lijst
+    Daarna krijg ik de Antwoord
+    De gok en de antwoorden worden in de Vorige gokken lijst toegevoegd (om te kijken hoe consistent de vogende gok zou zijn)
+    Als de Antwoord gelijk aan 4 :
+      Dan stopt de algoritme
+    Vogende gok is eigelijk de eerst vogende consistente gok van uit de Aantal moeglijkheden lijst
+    Enzo  tot de comnputer de code raad
+
+    """
+
+
     print("""==== Simpel Strategy==== """)
     print("=====(Gokken)===(Z)==(W)===== ")
     vorige_gokken = []
     #Hier bij beginen we met de eerste gok in de aantal moeglijkheden list
-    eerste_gok = Aantal_Moelijkhede()[0]         #AAAA
-    gok = eerste_gok
+       #AAAA
+    gok = Aantal_Moelijkhede()[0]
     pogingen = 0
     while True:
         zwartepen, wittepen = evaluate(gok, kleur_code)
@@ -108,6 +125,17 @@ def Simpel_strategy(kleur_code):
 
 
 def Worst_case_strategy(kleur_code):
+
+    """ Het Worst case Strategy werkt als volende :
+        Eerst geef ik de eerste gok altijd AABB om mijn Aantal moegelijkheden lijst te gaan beperken
+        Daarna krijg ik de Antwoord
+        De gok en de antwoorden worden in de Vorge gokken lijst toegevoegd (om te kijken hoe consistent de vogende gok zou zijn)
+        Als de Antwoord gelijk aan 4 :
+          Dan stopt de algoritme
+        Vogende gok is eigelijk de eerst vogende consistente gok van uit de Aantal moeglijkheden lijst
+        Enzo blijft tot de comnputer de code raad
+
+        """
     print("""=== Worst Case Strategy ===""")
     print("=====(Gokken)===(Z)==(W)===== ")
     vorige_gokken = []
@@ -128,7 +156,6 @@ def Worst_case_strategy(kleur_code):
             # Break ! we hebben de gehaime code gevonden !
             break  
         pogingen = pogingen + 1
-
         while True:
             # genereer een nieuwe willekeurige gok, consistent met
             # alle eerdere gokken !.
@@ -145,7 +172,25 @@ def Worst_case_strategy(kleur_code):
     print("==========================")
     return gok , pogingen
 
+
+
+
 def Eigen_Strategy(kleur_code):
+
+
+    """ Het Eien Strategy werkt als volende :
+           Eerst geef ik de eerste gok altijd ABCD om mijn Aantal moegelijkheden lijst te gaan beperken met 312 elmenten over in het worst case
+           Daarna krijg ik de Antwoord
+           De gok en de antwoorden worden in de Vorge gokken lijst toegevoegd (om te kijken hoe consistent de vogende gok zou zijn)
+           Als de Antwoord gelijk aan 4 :
+             Dan stopt de algoritme
+           Als de Antwoord gelijk aan 0 :
+             Dan verwijder ik de element van Aantal mogelijkheden lijst
+           Vogende gok is eigelijk de eerst vogende consistente gok van uit de Aantal moeglijkheden lijst
+           Enzo blijft tot de comnputer de code raad
+
+           """
+
     print("====Eigen Strategy=====")
     print("=====(Gokken)===(Z)==(W)===== ")
     vorige_gokken = []
@@ -190,35 +235,30 @@ def Eigen_Strategy(kleur_code):
 
 def Game():
     global kleur_code
-
-
+    spelen = True
     kleur_code = randomcode()
-    print(" --- MASTERMIND --- \n")
 
+    print(" --- MASTERMIND --- \n")
     print(" Welke Algorithem Wilt u het Zien ? \n")
     print("1. Simpel \n 2. Worst Case \n 3. Eigen \n")
-    alg = input("Voer hier de nummer in \n ")
-    if alg == 1 :
+    print("Raad de geheime kleurcode in zo min mogelijk pogingen.\n")
+    print("Voer uw kleurcode in. \n U kunt rood (A), groen (B), blauw (C), geel (D), wit (E) en roze (F) gebruiken.\n")
+    print("Het Gehiheme Code is ==( ",kleur_code ,")====")
+
+    alg = int(input("Voer hier de nummer in \n "))
+    if alg == 1:
         speler_gok, pogingen = Simpel_strategy(kleur_code)
 
-    elif alg == 2 :
+    elif alg == 2:
         speler_gok, pogingen = Worst_case_strategy(kleur_code)
     elif alg == 3:
         speler_gok, pogingen = Eigen_Strategy(kleur_code)
-    print("Raad de geheime kleurcode in zo min mogelijk pogingen.\n")
-    print("Voer uw kleurcode in. \n U kunt rood (A), groen (B), blauw (C), geel (D), wit (E) en roze (F) gebruiken.\n")
-
-
-    spelen = True
-
-    print("Het Gehiheme Code is ==( ",kleur_code ,")====")
 
     while spelen:
         b, w = 0, 0
         gokeset, secretset = set([]), set([])
-        speler_gok , pogingen = Eigen_Strategy(kleur_code)
-        if len(str(speler_gok)) != len(kleur_code):
 
+        if len(str(speler_gok)) != len(kleur_code):
             print(
                 "\nDe geheime code heeft precies vier kleuren. Ik weet het, je kunt tot vier tellen. Probeer het nog eens!")
             continue
@@ -226,7 +266,6 @@ def Game():
             if speler_gok[i] not in colors:
                 print("\nZoek op welke kleuren je in dit spel kunt gebruiken !!!!")
                 continue
-
         if b != 4 :
             for i in range(0, len(speler_gok)):
                 if speler_gok[i] == kleur_code[i]:
@@ -235,8 +274,6 @@ def Game():
                     gokeset.add(speler_gok[i])
                     secretset.add(kleur_code[i])
             feedback1 = b, len(gokeset.intersection(secretset))
-
-
 
         if b == 4:
             if pogingen == 1:
